1f527ccc9a7966547d1f20de424628ccd95faaec
ismailisik <i.isikl@outlook.com.tr>
2020-08-29 13:08:01 +0300

    Initial Project

---
+++ Bassic_RabbitMQ_Console/Consumer.RabbitMQ/Program.cs
@@ -0,0 +1,39 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace Consumer.RabbitMQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqp://localhost");
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare("Hello_RabbitMQ", false, false, false, null);
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    channel.BasicConsume("Hello_RabbitMQ", true, consumer);
+
+                    //Publisher tarafından gönderilemn messajları Recived event'ı ile dinlemeye başladık.
+                    consumer.Received += (model, ea) =>
+                    {
+                        var message = Encoding.UTF8.GetString(ea.Body.ToArray());
+                        Console.WriteLine($"Mesajınız: {message}");
+                    };
+
+                    Console.WriteLine("Çıkmak İçin Bir Tuşa Basınız...");
+                    Console.ReadLine();
+
+                }
+
+            }
+        }
+    }
+}
---
+++ Bassic_RabbitMQ_Console/Publisher.RabbitMQ/Program.cs
@@ -0,0 +1,34 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace Publisher.RabbitMQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri=new Uri("amqp://localhost");
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare("Hello_RabbitMQ", false, false, false, null);
+
+                    var MyMessage ="Bu RabbitMQ Test Messajıdır.";
+
+                    var bodyByte = Encoding.UTF8.GetBytes(MyMessage);
+
+                    channel.BasicPublish("", "Hello_RabbitMQ",null,bodyByte);
+
+                    Console.WriteLine("Messajınız İletildi");
+                }
+
+                Console.WriteLine("Çıkmak İçin Bir Tuşa Basınız...");
+                Console.ReadLine();
+            }
+        }
+    }
+}
